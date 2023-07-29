f19b5539b8522266f710f5c3e839d256cd7f8f6b
Mehzabin Haque <74411032+mehzabin-haque@users.noreply.github.com>
2023-07-29 13:33:17 +0600

    Create LinkedList.c

---
+++ LinkedList.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    struct node{
+    int data;
+    struct node *next;
+    };
+    struct node *head, *newnode ,*temp;
+    int choice=1, count=0;
+    head=0;
+    while(choice){
+    newnode = (struct node*) malloc(sizeof(struct node));
+
+        printf("Enter data  :\n");
+        scanf("%d",&newnode->data);
+        newnode->next=0;
+        if(head==0)
+        {
+            head=temp=newnode;
+        }
+        else
+        {
+            temp->next=newnode;
+            temp=newnode;
+        }
+        printf("Do you want to continue? (0,1)\n");
+        scanf("%d",&choice);
+    }
+    temp=head;
+    while(temp!=0)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+        count++;
+    }
+    printf("Total %d node is present in linked list.\n",count);
+    getch();
+
+}
